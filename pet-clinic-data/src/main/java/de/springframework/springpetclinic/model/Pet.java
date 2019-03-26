de1e7b6728c642f4ba98e73641b06faea8281af3
Anh Quang <QUang1994@web.de>
2019-03-26 11:29:34 +0100

    added entities. closes #26

--- pet-clinic-data/src/main/java/de/springframework/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/de/springframework/springpetclinic/model/Pet.java
@@ -4,10 +4,32 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
-@Data
 public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
 
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 }
