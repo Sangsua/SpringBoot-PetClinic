b1f4fbd413194981c099d5a33b5f7e160f0786b8
Anh Quang <QUang1994@web.de>
2019-03-27 15:31:14 +0100

    added  SpecialtyMapService and interface. Closes #31

---
+++ pet-clinic-data/src/main/java/de/springframework/springpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package de.springframework.springpetclinic.services;
+
+import de.springframework.springpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService <Specialty,Long> {
+}
