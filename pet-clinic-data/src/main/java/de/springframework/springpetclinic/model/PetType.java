d8ef60abd40cc3ce3cd9ee57d1efbd3591b98eef
Anh Quang <QUang1994@web.de>
2019-05-14 05:55:05 +0200

    lombok added and refactored model. Closes #51.

--- pet-clinic-data/src/main/java/de/springframework/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/de/springframework/springpetclinic/model/PetType.java
@@ -1,20 +1,19 @@
 package de.springframework.springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
     @Column(name = "type_name")
     private String typeName;
 
-    public String getTypeName() {
-        return typeName;
-    }
-
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
 }
